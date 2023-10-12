2992c8ca349b131927b53e957cfd344ee4eefff5
hemanthgangam <139945812+hemanthgangam@users.noreply.github.com>
2023-10-12 19:57:24 +0530

    Add files via upload

---
+++ StudentRegistation2/src/main/java/com/example/StudentRegistation/StudentRegistationApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudentRegistation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRegistationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRegistationApplication.class, args);
+	}
+
+}
